c9206acd41b7a985c0736c32d23e1d80dc5f1097
feliperomio <113053407+feliperomio@users.noreply.github.com>
2024-09-09 12:54:22 -0300

    Add files via upload

---
+++ Contador de letra/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,21 @@
+﻿string str = "meu nome é felipe romio.";
+
+// converte a mensagem em um array char
+char[] frase = str.ToCharArray();
+
+// Inverte os caracteres
+Array.Reverse(frase);
+int x = 0;
+
+// conta as letras o's
+    foreach (char i in frase) { 
+        if (i == 'o') {
+             x++; 
+             } 
+        }
+
+// converte devolta para string
+string mensagem = new String(frase);
+
+Console.WriteLine(mensagem);
+Console.WriteLine($"A letra 'o' se repete {x} vezes.");
