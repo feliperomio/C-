a75377b70f2f0ad438de3c6c29f57c57087fa431
feliperomio <113053407+feliperomio@users.noreply.github.com>
2024-09-26 21:16:06 -0300

    Add files via upload

---
+++ Calculadora/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program{
+
+    static void Main(string[] args) {
+        double resultado;
+
+
+        Console.WriteLine("Calculadora \n");
+
+        Console.WriteLine("Informe o primeiro número: ");
+        double primeiroNumero = Double.Parse(Console.ReadLine());
+     
+
+        Console.WriteLine("Informa a operação desejada: ");
+        string op = Console.ReadLine();
+
+        Console.WriteLine("Informe o segundo número: ");
+        double segundoNumero = Double.Parse(Console.ReadLine()); ;
+
+        switch (op)
+        {
+            case "-":
+                resultado = primeiroNumero - segundoNumero;
+                Console.WriteLine("Resultado: " + resultado);
+                break;
+
+            case "+":
+                resultado = primeiroNumero + segundoNumero;
+                Console.WriteLine("Resultado: " + resultado);
+                break;
+
+            case "*":
+                resultado = primeiroNumero * segundoNumero;
+                Console.WriteLine("Resultado: " + resultado);
+                break;
+
+            case "/":
+                resultado = primeiroNumero / segundoNumero;
+                Console.WriteLine("Resultado: " + resultado);
+                break;
+
+        }
+
+    }
+}
